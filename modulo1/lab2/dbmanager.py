79324d26e830a4e05143327512615344ccd709b9
richardyantas5 <richadyantas5@gmail.com>
2021-01-21 21:47:57 -0500

    some methods such as find_or_create, list and insert were moved to command class and a new method crud_commands was created

--- modulo1/lab2/dbmanager.py
+++ modulo1/lab2/dbmanager.py
@@ -1,3 +1,6 @@
+import command
+import requester
+import presenter
 import psycopg2.extras
 from rich.console import Console
 from rich.table import Table
@@ -5,44 +8,21 @@ from rich.table import Table
 console = Console()
 
 class DBManager:
-    
+
     def __init__(self):
         self.DB_connect = psycopg2.connect(dbname='flashback')
         self.DB = self.DB_connect.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
-        
-    def find_or_create(self, data):
-        query = f"SELECT * FROM commands WHERE commandline LIKE '{data['commandline']}';"
-        self.DB.execute(query)
-        if self.DB.fetchone():
-            return self.DB.fetchall()
-        else:
-            #query = f"INSERT INTO commands(category, commandline, description) VALUES('{data['category']}', '{data['commandline']}', '{data['description']}') RETURNING *;"
-            query = f"INSERT INTO commands(category, commandline, description) VALUES(LOWER('{data['category']}'), '{data['commandline']}', '{data['description']}'); SELECT * FROM commands WHERE category LIKE LOWER('{data['category']}');"
-            self.DB.execute(query)
-            self.DB_connect.commit()
-            return self.DB.fetchall()
              
-        
-    def list_ruby_ror(self, parameter=None):        
-        # ALTER TABLE commands ALTER COLUMN commandline TYPE varchar(100);
-        # \d commands; \dt commands;
-        # query = f"SELECT * FROM commands WHERE category LIKE '{data['category']}';"
-        query = f"SELECT * FROM commands WHERE category LIKE 'rails';"
-        self.DB.execute(query)
-        return self.DB.fetchall()      
-
-    def insert_ruby_ror(self,parameter=None):        
-        print("Entering Ruby command data: ")
-        #line = input("> ")
-        data = {}
-        data["category"] = input("> category: ")
-        data["commandline"] = input("> commandline: ")
-        data["description"] = input("> description: ")
-        data = self.find_or_create(data)        
-        return data
-
-    def english_language_list(self, parameter=None): return
-    def english_language_insert(self, parameter=None): return
+    def crud_commands(self, parameter=None):
+        command_ = command.Command()
+        presenter.print_menu(command_.MENU)
+        option = input("> ")
+        method = command_.MENU[option]["method"]
+        # command_.create()
+        # command_.update()
+        # command_.delete()
+        # command_.show()
+        return getattr(command_, method)()
 
 
-
+# python test/rich_table_syntax.py
