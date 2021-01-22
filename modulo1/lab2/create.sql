c2cf375025726d0dacde9af08f6857a100589ed2
richardyantas5 <richadyantas5@gmail.com>
2021-01-21 21:46:46 -0500

    Table ideas was added

--- modulo1/lab2/create.sql
+++ modulo1/lab2/create.sql
@@ -5,3 +5,10 @@ CREATE TABLE commands (
   commandline varchar(50),
   description varchar(200)
 );
+
+DROP TABLE IF EXISTS ideas CASCADE;
+CREATE TABLE ideas{
+  id serial primary key,
+  title varchar(25) not null,
+  description varchar(200)
+}
